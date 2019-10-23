2fe99e66a9f529794918ea12a4645823d52f1781
Justin Oliver Lee <JOLee83@JOLee83-MacBook-Air.local>
2019-10-23 13:31:36 -0700

    resume update

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,31 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 class Contact extends Component {
-  render() {
-    return (
-      <section className="contact">
-        <ul>
-          <li>
-            <i className="fas fa-map-marker-alt" /> Seattle, WA
-          </li>
-          <li>
-            <a href="tel://19044729516"><i className="fas fa-phone-square" /> (904) 472-9516</a>
-          </li>
-          <li>
-            <a href="mailto:lee.justin.oliver@gmail.com"><i className="fas fa-envelope-square" /> lee.justin.oliver@gmail.com</a>
-          </li>
-          <li>
-            <a href="https://www.justin-oliver.com"><i className="fas fa-globe" /> justin-oliver.com</a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/justin-oliver-lee/"><i className="fab fa-linkedin" /> justin-oliver-lee</a>
-          </li>
-          <li>
-            <a href="https://github.com/JOLee8"><i className="fab fa-github-square" /> JOLee83</a>
-          </li>
-        </ul>
-      </section>
-    );
-  }
+	render() {
+		return (
+			<section className="contact">
+				<ul>
+					<li>
+						<i className="fas fa-map-marker-alt" /> Seattle, WA
+					</li>
+					<li>
+						<a href="tel://19044729516">
+							<i className="fas fa-phone-square" /> (904) 472-9516
+						</a>
+					</li>
+					<li>
+						<a href="mailto:lee.justin.oliver@gmail.com">
+							<i className="fas fa-envelope-square" />{' '}
+							lee.justin.oliver@gmail.com
+						</a>
+					</li>
+					<li>
+						<a href="https://www.justin-oliver.com">
+							<i className="fas fa-globe" /> justin-oliver.com
+						</a>
+					</li>
+					<li>
+						<a href="https://www.linkedin.com/in/justin-oliver-lee/">
+							<i className="fab fa-linkedin" /> justin-oliver-lee
+						</a>
+					</li>
+					<li>
+						<a href="https://github.com/JOLee83">
+							<i className="fab fa-github-square" /> JOLee83
+						</a>
+					</li>
+				</ul>
+			</section>
+		)
+	}
 }
 
-export default Contact;
+export default Contact
--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,19 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 class Skills extends Component {
-  render() {
-    return (
-      <section className="skills">
-        <header className="sec-title">Skills</header>
-        <div className="skills-div">
-          <ul>
-            <li>Front-End and Back-End languages like HTML, CSS, JavaScript, C#, and SQL.</li>
-            <li>Front-End and Back-End frameworks like React, Gatsby, and .Net.</li>
-            <li>State and database management with Redux and Postgres.</li>
-            <li>Git for version control, familiar with RESTful and CRUD API architecture.</li>
-            <li>CSS preprocessors, libraries, and frameworks like SASS, LESS, Style Components, BootStrap, and Bulma.</li>
-            <li>Business tools and techniques like the Microsoft Office Suit along with Operations and Inventory Management.</li>
-          </ul>
-          {/* <div>
+	render() {
+		return (
+			<section className="skills">
+				<header className="sec-title">Skills</header>
+				<div className="skills-div">
+					<ul>
+						<li>
+							Front-End and Back-End languages like HTML, CSS, JavaScript, C#,
+							and SQL.
+						</li>
+						<li>
+							Front-End and Back-End frameworks like React, React Native, and
+							.Net.
+						</li>
+						<li>State and database management with Redux and Postgres.</li>
+						<li>
+							Git for version control, familiar with RESTful and CRUD API
+							architecture.
+						</li>
+						<li>
+							CSS preprocessors, libraries, and frameworks like SASS, LESS,
+							Style Components, BootStrap, and Bulma.
+						</li>
+						<li>
+							Business tools and techniques like the Microsoft Office Suit along
+							with Operations and Inventory Management.
+						</li>
+					</ul>
+					{/* <div>
             HTML
           </div>
           <div>
@@ -88,10 +103,10 @@ class Skills extends Component {
           <div>
             Inventory Management
           </div>*/}
-        </div>
-      </section>
-    );
-  }
+				</div>
+			</section>
+		)
+	}
 }
 
-export default Skills;
+export default Skills
